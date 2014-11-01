cc13fe29d6cd9902bdad0b4af5f7ccc6eb0a566b
Dave Briccetti <daveb@davebsoft.com>
2014-10-31 21:33:33 -0700

    Remove the second LED. Fine-tune the blink ranges.

--- range_blink/range_blink.ino
+++ range_blink/range_blink.ino
@@ -1,14 +1,12 @@
 #define trig 9
 #define echo 10
 #define led1 11
-#define led2 12
 float soundSpeedMicrosecondsPerCm = 29.387;
 
 void setup() {
   Serial.begin(9600);
   pinMode(trig, OUTPUT);
   pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
   pinMode(echo, INPUT);
 }
 
@@ -19,14 +17,12 @@ void loop() {
   unsigned long oneWayTripTimeMicroseconds = pulseIn(echo, HIGH) / 2;
   float cm = oneWayTripTimeMicroseconds / soundSpeedMicrosecondsPerCm;
   Serial.println(cm);
-  digitalWrite(led2, cm < 40 ? HIGH : LOW);
   int b;
-  if      (cm <  10) b = 0;
-  else if (cm <  20) b = 1;
-  else if (cm <  40) b = 2;
-  else if (cm < 100) b = 3;
-  else if (cm < 200) b = 4;
-  else if (cm < 400) b = 5;
+  if      (cm <  20) b = 0;
+  else if (cm <  40) b = 1;
+  else if (cm < 100) b = 2;
+  else if (cm < 200) b = 3;
+  else if (cm < 400) b = 4;
   else               b = 20;
   blink(b);
   delay(100);
